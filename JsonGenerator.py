d71c2959a5796f0bbda50710b07073ab7dbc9706
Charly1307 <82918675+idarkcharly@users.noreply.github.com>
2022-12-30 10:54:44 -0600

    Update JsonGenerator.py

--- JsonGenerator.py
+++ JsonGenerator.py
@@ -5,14 +5,12 @@ import json
 import os
 # Change the path
 file_path = os.path.join('/home/carlos/Escritorio/')
-
 print("================welcome================\n")
 print("Json file generator for Item Models\n")
-mod_id = str(input("Ingresa el modid: \n"))
+mod_id = str(input("Insert the modid: \n"))
 
 while True:
-
-    item_id = str(input("Ingresa el id del item: \n"))
+    item_id = str(input("Insert the item id: \n"))
     # Python dictionary
     item_models = {
         "parent": "item/generated",
@@ -22,10 +20,9 @@ while True:
     }
     # Insert the variables
     item_models["textures"]['layer0'] = mod_id + ':item/' + item_id
-
     # Serializing json
     json_object = json.dumps(item_models, indent=4)
-
     # Generate json file
     with open(file_path + item_id+'.json', "w") as outfile:
         outfile.write(json_object)
+        print("=======SUCCESS=======\n")
