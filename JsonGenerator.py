21e4ec89f438efa0541bd3d4bc628281a203a806
Charly1307 <82918675+idarkcharly@users.noreply.github.com>
2022-12-30 10:49:52 -0600

    Update JsonGenerator.py

--- JsonGenerator.py
+++ JsonGenerator.py
@@ -1,26 +1,31 @@
 # @Author : Charly1307
 # Version : 1.0
+# Loop to be stopped manual
 import json
 import os
 # Change the path
 file_path = os.path.join('/home/carlos/Escritorio/')
 
+print("================welcome================\n")
+print("Json file generator for Item Models\n")
 mod_id = str(input("Ingresa el modid: \n"))
-item_id = str(input("Ingresa el id del item: \n"))
 
-# Python dictionary
-item_models = {
-    "parent": "item/generated",
-    "textures": {
-        "layer0": ""
+while True:
+
+    item_id = str(input("Ingresa el id del item: \n"))
+    # Python dictionary
+    item_models = {
+        "parent": "item/generated",
+        "textures": {
+            "layer0": ""
+        }
     }
-}
-# Insert the variables
-item_models["textures"]['layer0'] = mod_id + ':item/' + item_id
+    # Insert the variables
+    item_models["textures"]['layer0'] = mod_id + ':item/' + item_id
 
-# Serializing json
-json_object = json.dumps(item_models, indent=4)
+    # Serializing json
+    json_object = json.dumps(item_models, indent=4)
 
-# Generate json file
-with open(file_path + item_id+'.json', "w") as outfile:
-    outfile.write(json_object)
+    # Generate json file
+    with open(file_path + item_id+'.json', "w") as outfile:
+        outfile.write(json_object)
